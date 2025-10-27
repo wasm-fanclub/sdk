0a6610b122ffe0aaf2823dc86055af282fdef0a4
Blue Falcon <anon@hostpi.onmicrosoft.com>
2025-10-27 03:05:27 -0700

    refactor: :construction: cleanup and align emsdk profile

--- init/build-emcc-profile.sh
+++ init/build-emcc-profile.sh
@@ -10,3 +10,25 @@ BASE_URL="https://raw.githubusercontent.com"
 DOCKERFILE_URL="$BASE_URL/$URL_PATH"
 
 PROFILE_SCRIPT=$(bash /opt/init/env-profile.sh "$PROFILE_NAME" "$DOCKERFILE_URL")
+
+EMSDK="/opt/emsdk"
+
+# Cleanup and set EMSDK path
+awk '
+/^export[[:space:]]+EMSDK=/ {
+  if (!done) {
+    print "export EMSDK=\"'"$EMSDK"'\"";
+    done = 1;
+  }
+  next;
+}
+{ print }
+' "${PROFILE_SCRIPT}" | tee "${PROFILE_SCRIPT}.tmp" > /dev/null
+mv "${PROFILE_SCRIPT}.tmp" "${PROFILE_SCRIPT}"
+
+echo "export CC=\"$EMSDK/upstream/emscripten/emcc\"" >> $PROFILE_SCRIPT
+echo "export CXX=\"$EMSDK/upstream/emscripten/em++\"" >> $PROFILE_SCRIPT
+echo "export LD=\"$EMSDK/upstream/emscripten/emcc\"" >> $PROFILE_SCRIPT
+echo "export AR=\"$EMSDK/upstream/emscripten/emar\"" >> $PROFILE_SCRIPT
+echo "export RANLIB=\"$EMSDK/upstream/emscripten/emranlib\"" >> $PROFILE_SCRIPT
+echo "export EMSCRIPTEN_SYSROOT=\"$EMSDK/upstream/emscripten/cache/sysroot\"" >> $PROFILE_SCRIPT
